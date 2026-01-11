79648f40b1f524a9ffb2aa6b9cbe35e7d7d51126
mnbst <47024101+mnbst@users.noreply.github.com>
2026-01-11 10:45:39 +0900

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -276,7 +276,7 @@ resource "google_cloud_run_v2_service" "green" {
 
       env {
         name  = "OAUTH_REDIRECT_URI"
-        value = var.domain_name != "" ? "https://${var.domain_name}" : "https://${google_compute_global_address.lb_ip.address}.nip.io"
+        value = var.domain_name != "" ? "https://${var.domain_name}/green" : "https://${google_compute_global_address.lb_ip.address}.nip.io/green"
       }
     }
 
