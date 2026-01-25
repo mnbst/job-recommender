948dda3d1f5b314b02614959f101f46d3745f7cd
mnbst <47024101+mnbst@users.noreply.github.com>
2026-01-25 22:26:05 +0900

    fix

--- services/components/__init__.py
+++ services/components/__init__.py
@@ -1,6 +1,5 @@
 """Streamlit custom components."""
 
 from services.components.cookie_manager import CookieManager, get_cookie_manager
-from services.components.redirect_button import redirect_button
 
-__all__ = ["CookieManager", "get_cookie_manager", "redirect_button"]
+__all__ = ["CookieManager", "get_cookie_manager"]
